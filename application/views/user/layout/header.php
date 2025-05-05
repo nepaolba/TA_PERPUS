69069bb40690065c8c05335d74ff9a3d84a2202e
nepaolba <nepaolba@gmail.com>
2025-05-05 20:39:01 +0800

    ubah menu header

--- application/views/user/layout/header.php
+++ application/views/user/layout/header.php
@@ -36,14 +36,12 @@
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-title">Logged in 5 min ago</div>
                             <a href="features-profile.html" class="dropdown-item has-icon">
-                                <i class="far fa-user"></i> Profile
+                                <i class="far fa-user"></i> Profil
                             </a>
-                            <a href="features-activities.html" class="dropdown-item has-icon">
-                                <i class="fas fa-bolt"></i> Activities
+                            <a href="<?= base_url('Welcome/daftarPinjam') ?>" class="dropdown-item has-icon">
+                                <i class="fas fa-bolt"></i> Transaksi
                             </a>
-                            <a href="features-settings.html" class="dropdown-item has-icon">
-                                <i class="fas fa-cog"></i> Settings
-                            </a>
+
                             <div class="dropdown-divider"></div>
                             <a href="<?= base_url('Auth') ?>" class="dropdown-item has-icon text-danger">
                                 <i class="fas fa-sign-out-alt"></i> LogIn
